32f55be6cebb8a7ec7c1d49926634787fc9a0db5
Aidas415 <aidasambotas@gmail.com>
2025-09-12 19:09:21 +0300

    Update yml and config files

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,16 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.env.USER_EMAIL = process.env.USER_EMAIL; 
+      config.env.USER_PHONE_NUMBER = process.env.USER_PHONE_NUMBER; 
+      config.env.USER_PASSWORD = process.env.USER_PASSWORD; 
+      config.env.DISPLAY_NAME = process.env.DISPLAY_NAME; 
+      try {
       const envConfig = JSON.parse(fs.readFileSync("cypress.env.json"));
       config.env = { ...config.env, ...envConfig };
+      }catch (err) {
+        console.warn("⚠️ cypress.env.json not found – skipping local env injection");
+      }
       return config;
     },
   },
